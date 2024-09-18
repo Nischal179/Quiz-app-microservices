288291a3be2888f32f795d60ea9d7d7a108a626b
Nischal876 <nischalaryal11@gmail.com>
2024-09-18 11:32:21 +0545

    Added code to seperate question-service as a standalone application from the quiz-app's code

---
+++ question-service/src/main/java/com/nischal/question_service/controller/QuestionController.java
@@ -0,0 +1,37 @@
+package com.nischal.question_service.controller;
+
+
+import com.nischal.question_service.model.Question;
+import com.nischal.question_service.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("question")
+public class QuestionController {
+
+    @Autowired
+    QuestionService questionService;
+
+    @GetMapping("/allQuestions")
+    public ResponseEntity<List<Question>> getAllQuestion() {
+        return questionService.getAllQuestions();
+    }
+
+    @GetMapping("/category/{category}")
+    public ResponseEntity<List<Question>> getQuestionByCategory(@PathVariable String category) {
+        return questionService.getQuestionByCategory(category);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<String> addQuestion(@RequestBody Question question) {
+        return questionService.addQuestion(question);
+    }
+
+    // generate
+    // getQuestions (questionId)
+    // getScore
+}
